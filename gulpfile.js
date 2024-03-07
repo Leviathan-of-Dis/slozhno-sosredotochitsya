a3c1103f838d960a4c770aeae3779c39c3219380
Leviathan of Dis <Leviathan-of-Dis@yandex.ru>
2024-03-08 00:26:52 +0300

    4d

--- gulpfile.js
+++ gulpfile.js
@@ -63,6 +63,12 @@ function fonts() {
             .pipe(browserSync.reload({stream: true}));
   }
 
+function scripts() {
+    return gulp.src('src/scripts/**/*.{js,json}')
+            .pipe(gulp.dest('dist/scripts'))
+            .pipe(browserSync.reload({stream: true}));
+  }
+
 function clean() {
     return del('dist');
   }
@@ -80,9 +86,10 @@ function watchFiles() {
     gulp.watch(['src/styles/**/*.css'], css);
     gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
     gulp.watch(['src/fonts/**/*.{css,woff,woff2}'], fonts);
+    gulp.watch(['src/scripts/**/*.{js}'], scripts);
 }
 
-const build = gulp.series(clean, gulp.parallel(html, css, images, videos, fonts));
+const build = gulp.series(clean, gulp.parallel(html, css, images, videos, fonts, scripts));
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
   exports.watchapp = watchapp;
@@ -93,5 +100,6 @@ const watchapp = gulp.parallel(build, watchFiles, serve);
   exports.images = images;
   exports.videos = videos;
   exports.fonts = fonts;
+  exports.scripts = scripts;
   
   exports.default = watchapp;
