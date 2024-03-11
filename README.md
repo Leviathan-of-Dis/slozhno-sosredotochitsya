06f6d33c47fe3e515ad931bf928ba23dfc9dc64e
Leviathan of Dis <Leviathan-of-Dis@yandex.ru>
2024-03-11 16:08:49 +0300

    Changes after first review

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 
-## [HTTPS ссылка на репозиторий](https://github.com/Leviathan-of-Dis/ono-tebe-nado.git)
+# [HTTPS ссылка на репозиторий](https://github.com/Leviathan-of-Dis/ono-tebe-nado.git)
+
+## Изменения после ревью
+1. Отключена работа HTML-minifier
+2. Отключена работа CSSnano
+(Надеюсь этого хватит чтобы было удобно проверить работу)
+3. Исправлены замечания по поводу расхождений с макетом.
 
   
--- gulpfile.js
+++ gulpfile.js
@@ -10,96 +10,97 @@ const cssnano = require('cssnano');
 const htmlMinify = require('html-minifier');
 
 function html() {
-    const options = {
-        removeComments: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        sortClassName: true,
-        useShortDoctype: true,
-        collapseWhitespace: true,
-        minifyCSS: true,
-        keepClosingSlash: true
-    };
-    return gulp.src('src/**/*.html')
-            .pipe(plumber())
-            .on('data', function(file) {
-                const buferFile = Buffer.from(htmlMinify.minify(file.contents.toString(), options))
-                return file.contents = buferFile
-            })
-            .pipe(gulp.dest('dist/'))
-            .pipe(browserSync.reload({stream: true}));
-  }
-
-function css() {
-    const plugins = [
-        autoprefixer(),
-        mediaquery(),
-        cssnano()
-    ]; 
-    return gulp.src('src/styles/**/*.css')
-            .pipe(plumber())
-            .pipe(concat('bundle.css'))
-            .pipe(postcss(plugins))
-            .pipe(gulp.dest('dist/'))
-            .pipe(browserSync.reload({stream: true}));
-  }
-  
-function images() {
-    return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
-            .pipe(gulp.dest('dist/images'))
-            .pipe(browserSync.reload({stream: true}));
-  }
-  
-function videos() {
-    return gulp.src('src/videos/**/*.{webm,mp4}')
-            .pipe(gulp.dest('dist/videos'))
-            .pipe(browserSync.reload({stream: true}));
-  }
-
-function fonts() {
-    return gulp.src('src/fonts/**/*.{css,woff,woff2}')
-            .pipe(gulp.dest('dist/fonts'))
-            .pipe(browserSync.reload({stream: true}));
-  }
-
-function scripts() {
-    return gulp.src('src/scripts/**/*.{js,json}')
-            .pipe(gulp.dest('dist/scripts'))
-            .pipe(browserSync.reload({stream: true}));
-  }
-
-function clean() {
-    return del('dist');
-  }
-
-function serve() {
-    browserSync.init({
-      server: {
-        baseDir: './dist'
-      }
-    });
-  }
-
-function watchFiles() {
-    gulp.watch(['src/**/*.html'], html);
-    gulp.watch(['src/styles/**/*.css'], css);
-    gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
-    gulp.watch(['src/fonts/**/*.{css,woff,woff2}'], fonts);
-    gulp.watch(['src/scripts/**/*.{js}'], scripts);
+  const options = {
+    removeComments: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    sortClassName: true,
+    useShortDoctype: true,
+    collapseWhitespace: true,
+    minifyCSS: true,
+    keepClosingSlash: true,
+  };
+  return gulp
+    .src('src/**/*.html')
+    .pipe(plumber())
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
-const build = gulp.series(clean, gulp.parallel(html, css, images, videos, fonts, scripts));
+function css() {
+  const plugins = [autoprefixer(), mediaquery()];
+  return gulp
+    .src('src/styles/**/*.css')
+    .pipe(plumber())
+    .pipe(concat('bundle.css'))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function images() {
+  return gulp
+    .src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
+    .pipe(gulp.dest('dist/images'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function videos() {
+  return gulp
+    .src('src/videos/**/*.{webm,mp4}')
+    .pipe(gulp.dest('dist/videos'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function fonts() {
+  return gulp
+    .src('src/fonts/**/*.{css,woff,woff2}')
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function scripts() {
+  return gulp
+    .src('src/scripts/**/*.{js,json}')
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function clean() {
+  return del('dist');
+}
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: './dist',
+    },
+  });
+}
+
+function watchFiles() {
+  gulp.watch(['src/**/*.html'], html);
+  gulp.watch(['src/styles/**/*.css'], css);
+  gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch(['src/fonts/**/*.{css,woff,woff2}'], fonts);
+  gulp.watch(['src/scripts/**/*.{js}'], scripts);
+}
+
+const build = gulp.series(
+  clean,
+  gulp.parallel(html, css, images, videos, fonts, scripts)
+);
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
-  exports.watchapp = watchapp;
-  exports.build = build;
-  exports.clean = clean;
-  exports.html = html;
-  exports.css = css;
-  exports.images = images;
-  exports.videos = videos;
-  exports.fonts = fonts;
-  exports.scripts = scripts;
-  
-  exports.default = watchapp;
+exports.watchapp = watchapp;
+exports.build = build;
+exports.clean = clean;
+exports.html = html;
+exports.css = css;
+exports.images = images;
+exports.videos = videos;
+exports.fonts = fonts;
+exports.scripts = scripts;
+
+exports.default = watchapp;
